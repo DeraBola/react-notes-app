ee9bc68cd1ac01b44b0e618d327df07f9bc517aa
DeraBola <96840776+DeraBola@users.noreply.github.com>
2022-08-01 13:12:33 +0100

    commit

---
+++ src/components/AddNote.js
@@ -0,0 +1,16 @@
+ 
+const AddNote  = () => {
+return(
+    <div className="note new">
+         <textarea 
+         rows="8" cols="10" placeholder='Type new add a note...' >
+         </textarea>
+         <div  className='note-footer' >
+             <small>200 remaining</small>
+             <button  className='save'>Save</button>
+         </div>
+    </div>
+);
+};
+
+export default AddNote ;
